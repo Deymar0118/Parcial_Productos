d58e58e9ab1c9c0a97fa9c04109451373ed63ae7
unknown <dampudia@unipacifico.edu.co>
2024-03-19 10:16:33 -0500

    se hicieron correcciones en el metodo buscar

--- Registrar_Productos.cpp
+++ Registrar_Productos.cpp
@@ -34,23 +34,23 @@
 // Se soluciono el problema del metodo "Calcular promedio de precios"
 // Los problemas con el metodo "buscar producto" todavia persisten. (solucionar en lo mas pronto posiblse).
 
-//Notas del Lic. Pérez al Ing. Ampudia:
-//Se soluciono el problema con el método buscar, era necesario colocarlo por fuera del for, ya que al estar adentro del for, mostraría si o si el mensaje 
+// Notas del Lic. Pérez al Ing. Ampudia:
+// Se soluciono el problema con el método buscar, era necesario colocarlo por fuera del for, ya que al estar adentro del for, mostraría si o si el mensaje
 //"El Producto correspondiente al ID ingresado no se encuentra registrado en el inventario". El problema era que mostraba ese mensaje a pesar que, el prodcuto estuviera registrado
-//Finalmente, y depues de tanta lucha, la funcion eliminar esta funcionando bien, tecnicamente, la unica cosa es que no lograba organizarlo de manera que aun asi mostrase los
-//datos del ID del producto ingresado. Se reorganizo la funcion "switch" para que no existiera inconvenientes en su uso.
-//Falta solamente solucionar los problemas de cierre de programa, luego los revisaré poco a poco aver donde están. 
+// Finalmente, y depues de tanta lucha, la funcion eliminar esta funcionando bien, tecnicamente, la unica cosa es que no lograba organizarlo de manera que aun asi mostrase los
+// datos del ID del producto ingresado. Se reorganizo la funcion "switch" para que no existiera inconvenientes en su uso.
+// Falta solamente solucionar los problemas de cierre de programa, luego los revisaré poco a poco aver donde están.
 
 // Nuevas notas del Lic. Pérez al Ing. Ampudia:
-// Ahora, el código emitirá un mensaje en caso de que se registre un producto con un ID ya utilizado, en caso de que el ID no sea duplicado, le dajará al usuario 
-//continuar con la ejecución del programa. En caso contrario, le dirá que el ID está en uso y que lo intente nuevamente
+// Ahora, el código emitirá un mensaje en caso de que se registre un producto con un ID ya utilizado, en caso de que el ID no sea duplicado, le dajará al usuario
+// continuar con la ejecución del programa. En caso contrario, le dirá que el ID está en uso y que lo intente nuevamente
 
-#include <iostream>
-#include <malloc.h>
+#include <iostream> // permite la entrada y salida de datos
+#include <malloc.h> // permite la asignacion dinamica de memoria
 
-using namespace std;
+using namespace std; // permite usar los metodos de la libreria std
 
-struct RegistroProducto
+struct RegistroProducto // Estructura de registro de productos.
 {
     int ID = 0;
     char Nombre[30];
@@ -68,13 +68,13 @@ int registro() // Por este lado no hay problema.
 {
     if (cab == NULL)
     {
-        cab = (struct RegistroProducto *)malloc(sizeof(struct RegistroProducto));
-
-        cout << "Ingrese el nombre del producto: ";
-        cin >> cab->Nombre;
+        cab = (struct RegistroProducto *)malloc(sizeof(struct RegistroProducto)); // Se asigna la memoria dinamica para el registro de productos.
 
         cout << "Ingrese el ID del producto: ";
         cin >> cab->ID;
+
+        cout << "Ingrese el nombre del producto: ";
+        cin >> cab->Nombre;
 
         cout << "Ingrese el valor unitario del producto: $";
         cin >> cab->ValorUnitario;
@@ -88,48 +88,54 @@ int registro() // Por este lado no hay problema.
     else
     {
         aux = (struct RegistroProducto *)malloc(sizeof(struct RegistroProducto));
-    cout << "Ingrese el ID del producto: ";
-    cin >> aux->ID;
+        cout << "Ingrese el ID del producto: ";
+        cin >> aux->ID;
 
-    // A partir de aquí se usan los datos Boolean
-    bool idExists = false;
-    aux2 = cab;
-    while (aux2 != NULL) {
-      if (aux2->ID == aux->ID) {
-        idExists = true;
-        break;
-      }
-      aux2 = aux2->sig;
-    }
-    if (idExists) {
-      cout << "El ID ingresado ya esta en uso. Por favor, intente nuevamente."
-           << endl;
-      free(aux);
-    } else {
+        // A partir de aquí se usan los datos Boolean
+        bool idExists = false;
+        aux2 = cab;
+        while (aux2 != NULL)
+        {
+            if (aux2->ID == aux->ID)
+            {
+                idExists = true;
+                break;
+            }
+            aux2 = aux2->sig;
+        }
+        if (idExists)
+        {
+            cout << "El ID ingresado ya esta en uso. Por favor, intente nuevamente."
+                 << endl;
+            free(aux);
+        }
+        else
+        {
 
-      cout << "Ingrese el nombre del producto: ";
-      cin >> aux->Nombre;
+            cout << "Ingrese el nombre del producto: ";
+            cin >> aux->Nombre;
 
-      cout << "Ingrese el valor unitario del producto: $";
-      cin >> aux->ValorUnitario;
+            cout << "Ingrese el valor unitario del producto: $";
+            cin >> aux->ValorUnitario;
 
-      cout << "Ingrese la cantidad a registrar del producto: ";
-      cin >> aux->Cantidad;
+            cout << "Ingrese la cantidad a registrar del producto: ";
+            cin >> aux->Cantidad;
 
-      aux->sig = NULL;
+            aux->sig = NULL;
 
-      aux2 = cab;
-      while (aux2->sig != NULL) {
-        aux2 = aux2->sig;
-      }
+            aux2 = cab;
+            while (aux2->sig != NULL) // Se busca el ultimo registro de productos.
+            {
+                aux2 = aux2->sig;
+            }
 
-      aux->acumulador = aux2->acumulador + 1;
-      aux2->sig = aux;
-      aux = NULL;
-      aux2 = NULL;
+            aux->acumulador = aux2->acumulador + 1;
+            aux2->sig = aux; // Se agrega el nuevo registro de productos al final de la lista.
+            aux = NULL;
+            aux2 = NULL;
 
-        free(aux);
-        free(aux2);
+            free(aux);
+            free(aux2);
         }
     }
     return 0;
@@ -148,8 +154,8 @@ int mostrar() // Funciona correctamente.
     }
     return 0;
 }
-            
-int buscar() //Metodo corregido y funcionando correctamente, era necesario tan solo, cambiar de lugar el cout y ponerlo fuera del for
+
+int buscar() // Metodo corregido y funcionando correctamente, era necesario tan solo, cambiar de lugar el cout y ponerlo fuera del for
 {
     int buscador = 0;
     cout << "Ingrese el ID del producto a buscar: ";
@@ -169,32 +175,45 @@ int buscar() //Metodo corregido y funcionando correctamente, era necesario tan s
             return 0;
         }
     }
-    cout << "El Producto correspondiente al ID ingresado no se encuentra registrado en el inventario" << endl;
+    if (buscador != aux->ID)
+    {
+        cout << "El Producto correspondiente al ID ingresado no se encuentra registrado en el inventario" << endl;
+        return 0;
+    }
     return 0;
 }
 
-int eliminar() {  //Finalmente, Este metodo esta tecnicamente finalizado, la logica es facil, codificarlo fue un verdadero infierno
-  int elemento = 0;
-  cout << "Ingrese el ID del producto que desea eliminar del inventario: ";
-  cin >> elemento;
-  aux = cab;
-  RegistroProducto *prev = NULL;
-  while (aux != NULL && aux->ID != elemento) {
-    prev = aux;
-    aux = aux->sig;
-  }
-  if (aux != NULL) {
-    if (prev == NULL) {
-      cab = aux->sig;
-    } else {
-      prev->sig = aux->sig;
+int eliminar()
+{ // Finalmente, Este metodo esta tecnicamente finalizado, la logica es facil, codificarlo fue un verdadero infierno
+    int elemento = 0;
+    cout << "Ingrese el ID del producto que desea eliminar del inventario: ";
+    cin >> elemento;
+    aux = cab;
+    RegistroProducto *prev = NULL;
+    while (aux != NULL && aux->ID != elemento)
+    { // verifica si el ID ingresado existe en el inventario
+        prev = aux;
+        aux = aux->sig;
     }
-    free(aux);
-    cout << "Producto eliminado exitosamente." << endl;
-  } else {
-    cout << "No existe un producto con el ID ingresado." << endl;
-  }
-  return 0;
+    if (aux != NULL)
+    {
+
+        if (prev == NULL)
+        {
+            cab = aux->sig;
+        }
+        else
+        {
+            prev->sig = aux->sig;
+        }
+        free(aux);
+        cout << "Producto eliminado exitosamente." << endl;
+    }
+    else
+    {
+        cout << "No existe un producto con el ID ingresado." << endl;
+    }
+    return 0;
 }
 
 int PrecioTotal() // Funciona correctamente
@@ -265,7 +284,7 @@ int vender() // metodo completo
             {
                 aux->Cantidad = aux->Cantidad - cantVender;
 
-                cout << "Venta relaizada con exito. mostrando resultados: " << endl;
+                cout << "Venta realizada con exito. mostrando resultados: " << endl;
 
                 cout << "producto existente en el inventario" << endl;
                 cout << "Producto #" << aux->acumulador + 1 << endl;
@@ -323,7 +342,7 @@ int main()
             buscar();
             break;
 
-        case 4: 
+        case 4:
             eliminar();
             break;
 
